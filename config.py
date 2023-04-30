a6a561427197ff9a59d6c8c91fe894ab0d355e20
Lucifergod507 <131298140+Lucifergod507@users.noreply.github.com>
2023-05-01 01:27:17 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6111909615:AAF6DEK8z6COeOf4djfV7tXJMpjjYQo2TSk")
+    API_ID = int(os.environ["API_ID", 29152063]
+    API_HASH = os.environ["API_HASH", "df3207f0fe395c1a79919d7eb311c8e4"]
+    AUTH_USERS = "5760012562 """
+
