4094c2eefdf00680f6ae2643cdb6154e721b49b7
Lucifergod507 <131298140+Lucifergod507@users.noreply.github.com>
2023-05-02 01:36:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def exec(cmd):
   
   
   
-@bot.on_message(filters.command(["start"]))&  (filters.chat(sudo_groups)))
+@bot.on_message(filters.command(["start"])&  (filters.chat(sudo_groups)))
 async def account_login(bot: Client, m: Message):
  editable = await m.reply_text("**Hello Mr./Mrs. I'm Ready to use \nSend /txt to extract video and for classplus send /clps  for /dhurina for /vision**")
 
